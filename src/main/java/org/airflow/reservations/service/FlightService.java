9e567fef26bd6f433fad48e127fafefd553ea770
Andres <andresfgomezd@gmail.com>
2025-06-15 12:55:36 -0500

    Fix: corrige el issue #123 sin pruebas unitarias

---
+++ src/main/java/org/airflow/reservations/service/FlightService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.airflow.reservations.service;
+
+/**
+ *
+ * @author andres
+ */
+public class FlightService {
+    
+}
